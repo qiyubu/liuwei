08c62b5cdaa8558154c2557f2dfde2f0f4463c63
qiyubu <qi.yubu@h3c.com>
2018-06-22 16:14:49 +0800

    first commit

---
+++ src/view/about.vue
@@ -0,0 +1,35 @@
+<template>
+     <el-carousel height="80vh" id="carousel">
+       <el-carousel-item>
+         <img src="../assets/index2.jpg" alt="">
+       </el-carousel-item>
+       <el-carousel-item>
+         <img src="../assets/index3.jpg" alt="">
+       </el-carousel-item>
+       <el-carousel-item>
+       <img src="../assets/index4.jpg" alt="">
+     </el-carousel-item>
+       <el-carousel-item>
+       <img src="../assets/index1.jpg" alt="">
+     </el-carousel-item>
+     </el-carousel>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '关于我们'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#carousel{
+  img{
+    width: 100%;
+    display: block;
+  }
+}
+</style>
---
+++ src/view/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="index">
+    <div class="menuWrap">
+      <div class="indexMenu">
+        <img src="../assets/logo-img.png" alt="" class="logo">
+        <el-menu
+          :default-active="activeIndex2"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="about">关于我们</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">服务内容</template>
+            <el-menu-item index="2-1">内容1</el-menu-item>
+            <el-menu-item index="2-2">内容2</el-menu-item>
+            <el-menu-item index="2-3">内容3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">内容4</template>
+              <el-menu-item index="2-4-1">内容1</el-menu-item>
+              <el-menu-item index="2-4-2">内容2</el-menu-item>
+              <el-menu-item index="2-4-3">内容3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">案列</template>
+            <el-menu-item index="3-1">案例1</el-menu-item>
+            <el-menu-item index="3-2">案例2</el-menu-item>
+            <el-menu-item index="3-3">案例3</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="4">拾途观点</el-menu-item>
+          <el-menu-item index="5">排行榜</el-menu-item>
+          <!--<el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
+        </el-menu>
+      </div>
+    </div>
+    <el-container>
+      <router-view></router-view>
+      <el-footer class="footer">11</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex2: 'about'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .menuWrap{
+    background-color: rgb(84, 92, 100);
+  }
+.indexMenu{
+  width: 1200px;
+  margin: 0 auto;
+  padding-left: 120px;
+  position: relative;
+  .logo{
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 60px;
+  }
+}
+</style>
